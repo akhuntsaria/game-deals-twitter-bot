4e5da09bcd03e5db988922db3d943ead2d10ee2c
akhuntsaria <akakikhuntsaria97@gmail.com>
2020-07-26 23:47:27 +0400

    Add test for the Reddit API

--- src/test/java/com/github/akhuntsaria/reddittotwitterbot/service/RedditServiceTest.java
+++ src/test/java/com/github/akhuntsaria/reddittotwitterbot/service/RedditServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -65,5 +66,27 @@ public class RedditServiceTest {
         when(postHistoryService.postHistoryEntryExists(redditPost.getName())).thenReturn(false);
 
         assertThat(redditServiceSpy.getFilteredPost()).isEqualTo(redditPost);
+    }
+
+    @Test
+    public void testGetNewPosts() {
+        // Mocking the configuration for fetching posts
+        when(botConfiguration.getUserAgent()).thenReturn("Reddit to Twitter Bot Test");
+        when(botConfiguration.getSubreddit()).thenReturn("announcements"); // The most popular subreddit
+        when(botConfiguration.getLimitOfPostsToDownload()).thenReturn(1); // Changing to 1 for performance (default is 100)
+
+
+        List<RedditPost> redditPostList = redditService.getNewPosts();
+        assertThat(redditPostList.size()).isEqualTo(1);
+
+        // Make sure fields which are used in this application are not not or empty
+        RedditPost redditPost = redditPostList.get(0);
+        assertThat(redditPost.getData()).isNotNull();
+        assertThat(redditPost.getCreated()).isGreaterThan(0);
+        assertThat(redditPost.getName()).hasSizeGreaterThan(0);
+        assertThat(redditPost.getPermalink()).hasSizeGreaterThan(0);
+        // Assuming post in the most popular subreddit can't practically have 0 score
+        assertThat(redditPost.getScore()).isNotEqualTo(0);
+        assertThat(redditPost.getTitle()).hasSizeGreaterThan(0);
     }
 }
